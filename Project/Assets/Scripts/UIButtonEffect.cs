8d37ee7f4db04618220fd00129795487c0d6e95a
ketoohome <35617875+ketoohome@users.noreply.github.com>
2018-11-24 19:30:28 +0800

    Test Vibration

---
+++ Project/Assets/Scripts/UIButtonEffect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIButtonEffect : MonoBehaviour {
+
+    public GameObject _Effect;
+    public float _Interval = 1.0f, _Duration = 3.0f;
+	// Use this for initialization
+	void OnEnable () {
+        StartCoroutine(CreateNode());
+	}
+
+    // TODO : Create a ImageNode by sometime
+    IEnumerator CreateNode() {
+        yield return new WaitForSeconds(1.0f);
+        while (true) {
+            GameObject node = GameObject.Instantiate<GameObject>(_Effect, _Effect.transform.parent);
+            StartCoroutine(UpdateNode(node));
+            yield return new WaitForSeconds(_Interval);
+        }
+    }
+
+    // TODO : Update a Node
+    IEnumerator UpdateNode(GameObject obj) {
+        obj.SetActive(true);
+        yield return new WaitForSeconds(_Duration);
+        Destroy(obj);
+    }
+
+}
---
+++ Project/Assets/Scripts/UIRoot.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TOOL;
+using GameCommon;
+
+public class UIRoot : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        InitilizeState();
+
+
+    }
+
+    void OnDestroy()
+    {
+
+    }
+
+
+
+    #region State
+    enum state{
+        Menu,
+        Game,
+        End
+    }
+
+    IStateMachine<UIRoot> mStateMechine;
+
+    void InitilizeState() {
+        mStateMechine = new IStateMachine<UIRoot>(this);
+        mStateMechine.Add(state.Menu, new StateMenu());
+        mStateMechine.Add(state.Game, new StateGame());
+        mStateMechine.Add(state.End, new StateEnd());
+
+        mStateMechine.SetCurrentState(state.Menu);
+    }
+    class StateMenu : IState<UIRoot>
+    {
+        public override void Enter(UIRoot root) {
+            _Root = root;
+            EventMachine.Register(EventID.EventID_FishingRod, OnFishingRod);
+            root.transform.Find("Menu").gameObject.SetActive(true);
+        }
+
+        public override void Exit(UIRoot root)
+        {
+            EventMachine.Unregister(EventID.EventID_FishingRod, OnFishingRod);
+            root.transform.Find("Menu").gameObject.SetActive(false);
+        }
+
+        void OnFishingRod(params object[] args)
+        {
+            if ((bool)args[0]) _Root.mStateMechine.ChangeState(state.Game);
+        }
+        UIRoot _Root;
+
+    }
+
+    class StateGame : IState<UIRoot>
+    {
+        public override void Enter(UIRoot root)
+        {
+            _Root = root;
+            root.transform.Find("Game").gameObject.SetActive(true);
+            //EventMachine.Register(EventID.EventID_FishingRod, OnFishingRod);
+        }
+
+        public override void Exit(UIRoot root)
+        {
+            root.transform.Find("Game").gameObject.SetActive(false);
+            //EventMachine.Unregister(EventID.EventID_FishingRod, OnFishingRod);
+        }
+
+        void OnFishingRod(params object[] args)
+        {
+            
+        }
+        UIRoot _Root;
+    }
+
+    class StateEnd : IState<UIRoot>
+    {
+        public override void Enter(UIRoot root)
+        {
+            root.transform.Find("End").gameObject.SetActive(true);
+        }
+
+        public override void Exit(UIRoot root)
+        {
+            root.transform.Find("End").gameObject.SetActive(false);
+        }
+    }
+    #endregion
+}
