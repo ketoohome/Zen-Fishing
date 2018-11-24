31a7120d145401fbe564276bbd7600f7750a6258
ketoohome <35617875+ketoohome@users.noreply.github.com>
2018-11-24 22:32:41 +0800

    update

---
+++ Project/Assets/Scripts/VibTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class VibTest : MonoBehaviour {
+
+    public void OnChangeScene() {
+        if(SceneManager.GetActiveScene().name != "NiceVibrationsDemo")
+            SceneManager.LoadScene("NiceVibrationsDemo");
+        else SceneManager.LoadScene("SampleScene");
+    }
+}
